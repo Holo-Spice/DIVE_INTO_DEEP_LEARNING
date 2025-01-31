4a14f05bf6c3621fa75e1198a14d37ab96ba013e
小蛋糕今天吃什么 <2927970764@qq.com>
2025-01-31 16:58:11 +0800

    Add 3-6.py

---
+++ Linear Regression/3-6.py
@@ -0,0 +1,16 @@
+import torch
+from IPython import display
+from tools.utils import load_data_fashion_mnist
+
+batch_size = 256
+train_iter, test_iter = load_data_fashion_mnist(batch_size=batch_size)
+
+num_inputs = 784  # 28*28的图像展开
+num_outputs = 10
+
+w = torch.normal(0, 0.01, size=(num_inputs, num_outputs), requires_grad=True)
+b = torch.zeros(num_outputs, requires_grad=True)
+
+X = torch.tensor([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+X.sum(0, keepdim=True)
+X.sum(1, keepdim=True)
