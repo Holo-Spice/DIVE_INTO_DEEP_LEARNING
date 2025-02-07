3925121eb2a3ebc83aaf58bef75c6803c7a75908
小蛋糕今天吃什么 <2927970764@qq.com>
2025-02-07 11:57:36 +0800

    fnish Linear neural network

--- Linear Regression/3-7.py
+++ Linear Regression/3-7.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 from tools.utils import load_data_fashion_mnist
+from tools.utils import train_ch3
 
 def init_weights(m):
     if type(m) == nn.Linear:
@@ -17,5 +18,12 @@ def main():
     # 初始化权重函数应用到每一层
     net.apply(init_weights)
 
+    # 定义损失函数
+    loss = nn.CrossEntropyLoss(reduction='none')
+    # 定义优化器
+    trainer = torch.optim.SGD(net.parameters(), lr = 0.1)
+
+    num_epochs = 10
+    train_ch3(net, train_iter, test_iter, loss, num_epochs, trainer)
 if __name__ == '__main__':
     main()
