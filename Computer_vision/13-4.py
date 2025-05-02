3d2411638abd337f59a85bd207df9e479753129b
小蛋糕今天吃什么 <2927970764@qq.com>
2025-05-02 15:10:45 +0800

    NMS

--- Computer_vision/13-4.py
+++ Computer_vision/13-4.py
@@ -32,7 +32,7 @@ def main():
 
     fig = d2l.plt.imshow(img)
     d2l.show_bboxes(fig.axes, ground_truth[:, 1:] * bbox_scale, ['dog', 'cat'], 'k')
-    d2l.show_bboxes(fig.axes, anchors * bbox_scale, ['0', '1', '2', '3', '4']);
+    d2l.show_bboxes(fig.axes, anchors * bbox_scale, ['0', '1', '2', '3', '4'])
     d2l.plt.show()
 
     labels = d2l.multibox_target(anchors.unsqueeze(dim=0),
@@ -40,6 +40,30 @@ def main():
 
     print(labels)
 
+    """NMS"""
+    anchors = torch.tensor([[0.1, 0.08, 0.52, 0.92], [0.08, 0.2, 0.56, 0.95],
+                            [0.15, 0.3, 0.62, 0.91], [0.55, 0.2, 0.9, 0.88]])
+    offset_preds = torch.tensor([0] * anchors.numel())
+    cls_probs = torch.tensor([[0] * 4,  # 背景的预测概率
+                              [0.9, 0.8, 0.7, 0.1],  # 狗的预测概率
+                              [0.1, 0.2, 0.3, 0.9]])  # 猫的预测概率
+    fig = d2l.plt.imshow(img)
+    d2l.show_bboxes(fig.axes, anchors * bbox_scale,
+                ['dog=0.9', 'dog=0.8', 'dog=0.7', 'cat=0.9'])
+    d2l.plt.show()
 
+    output = d2l.multibox_detection(cls_probs.unsqueeze(dim=0),
+                                offset_preds.unsqueeze(dim=0),
+                                anchors.unsqueeze(dim=0),
+                                nms_threshold=0.5)
+    print(output)
+
+    fig = d2l.plt.imshow(img)
+    for i in output[0].detach().numpy():
+        if i[0] == -1:
+            continue
+        label = ('dog=', 'cat=')[int(i[0])] + str(i[1])
+        d2l.show_bboxes(fig.axes, [torch.tensor(i[2:]) * bbox_scale], label)
+    d2l.plt.show()
 if __name__ == '__main__':
     main()
